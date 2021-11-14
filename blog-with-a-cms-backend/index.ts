708f3ec10ef157546c95e0ac7b9662622d85f10f
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2021-11-14 23:39:08 +0200

    add mongoose

--- blog-with-a-cms-backend/index.ts
+++ blog-with-a-cms-backend/index.ts
@@ -1,9 +1,38 @@
+import * as dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 
+dotenv.config();
 const app = express();
 const PORT = 8000;
 
-app.get('/', (req, res) => res.send('Hello World'));
+main().catch((err) => console.log(err));
+
+async function main() {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  await mongoose.connect(process.env.MONGODB_CONNECTION_STRING!);
+  const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    role: String,
+    summary: String,
+  });
+  const User = mongoose.model('User', userSchema);
+  app.get('/', async (req, res) => {
+    const user = new User({
+      firstName: 'String3',
+      lastName: 'String',
+      email: 'String',
+      password: 'String',
+      role: 'String',
+      summary: 'String',
+    });
+    await user.save();
+    res.send(user);
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
