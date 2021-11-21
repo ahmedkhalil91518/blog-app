e82d5ba0d81ce0c643bd2bfb3b7f14361083c3b6
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2021-11-22 00:44:25 +0200

    remove non null assertion exception

--- blog-with-a-cms-backend/index.ts
+++ blog-with-a-cms-backend/index.ts
@@ -17,7 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 main().catch((err) => console.log(err));
 
 async function main() {
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   await mongoose.connect(process.env.MONGODB_CONNECTION_STRING!);
 
   app.post('/api/create-user', userController.createUser);
