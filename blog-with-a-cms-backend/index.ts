6c24019b58a88fa56f7e1063cf59cbf3ef5fc714
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2021-11-30 12:23:41 +0200

    make the articles service fetch data from the backend

--- blog-with-a-cms-backend/index.ts
+++ blog-with-a-cms-backend/index.ts
@@ -1,16 +1,18 @@
-import * as dotenv from 'dotenv';
-import express from 'express';
-import mongoose from 'mongoose';
-import * as userController from './controllers/user.controller';
-import * as tagController from './controllers/tag.controller';
-import * as articleController from './controllers/article.controller';
-import * as commentController from './controllers/comment.controller';
-import * as ratingController from './controllers/rating.controller';
+import * as dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import * as userController from "./controllers/user.controller";
+import * as tagController from "./controllers/tag.controller";
+import * as articleController from "./controllers/article.controller";
+import * as commentController from "./controllers/comment.controller";
+import * as ratingController from "./controllers/rating.controller";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -19,16 +21,18 @@ main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(process.env.MONGODB_CONNECTION_STRING!);
 
-  app.post('/api/create-user', userController.createUser);
-  app.post('/api/create-tag', tagController.createTag);
-  app.post('/api/create-article', articleController.createArticle);
-  app.post('/api/create-comment', commentController.createComment);
-  app.post('/api/create-rating', ratingController.createRating);
-  app.get('/api/read-article/:articleId', articleController.readArticle);
-  app.get('/api/read-all-articles-with-a-certain-tag/', articleController.readAllArticlesWithACertainTag);
-  app.get('/api/read-user/:userId', userController.readUser);
-  app.get('/api/read-all-articles/', articleController.readAllArticles);
-
+  app.post("/api/create-user", userController.createUser);
+  app.post("/api/create-tag", tagController.createTag);
+  app.post("/api/create-article", articleController.createArticle);
+  app.post("/api/create-comment", commentController.createComment);
+  app.post("/api/create-rating", ratingController.createRating);
+  app.get("/api/read-article/:articleId", articleController.readArticle);
+  app.get(
+    "/api/read-all-articles-with-a-certain-tag/",
+    articleController.readAllArticlesWithACertainTag
+  );
+  app.get("/api/read-user/:userId", userController.readUser);
+  app.get("/api/read-all-articles/", articleController.readAllArticles);
 }
 
 app.listen(PORT, () => {
--- blog-with-a-cms-frontend/src/app/articles.service.ts
+++ blog-with-a-cms-frontend/src/app/articles.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticlesService {
+  apiUrl = environment.apiUrl;
   constructor(private http: HttpClient) {}
 
   getAllArticles() {
-    return this.http.get('assets/articles.json');
+    return this.http.get(this.apiUrl + "/api/read-all-articles");
   }
 }
